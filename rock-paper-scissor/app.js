7ebe8f96b367e0dc4b8436b8ca264ca4cd3ea6fc
Depkstha <dharmarajshrestha9870@gmail.com>
2025-01-07 01:40:25 +0545

    implement game logic and UI for Rock Paper Scissors with score tracking and messages

--- rock-paper-scissor/app.js
+++ rock-paper-scissor/app.js
@@ -1,11 +1,63 @@
 let userScore = 0;
 let computerScore = 0;
 
+//choice options
+const OPTIONS = ["rock", "paper", "scissors"];
+
+//winning logic
+const RULES = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
 const choices = document.querySelectorAll(".choice");
+const userScoreBoard = document.getElementById("user-score");
+const computerScoreBoard = document.getElementById("computer-score");
+const message = document.getElementById("message");
 
 choices.forEach((choice) => {
-    choice.addEventListener("click", (event) => {
-      console.log(event.currentTarget);
-      console.log(event.type);
+  choice.addEventListener("click", (event) => {
+    const userChoice = event.currentTarget.id;
+    playGame(userChoice);
   });
 });
+
+const getComputerChoice = () => {
+  const randomIndex = Math.floor(Math.random() * OPTIONS.length);
+  return OPTIONS[randomIndex];
+};
+
+const gameDraw = (userChoice) => {
+  message.innerText = `It's a draw. You both chose ${userChoice}. Play again!`;
+  message.style.backgroundColor = "#081b31";
+  return;
+};
+
+const decideWinner = (userChoice, computerChoice) => {
+  return RULES[userChoice] === computerChoice ? "user" : "computer";
+};
+
+const showWinner = (winner, userChoice, computerChoice) => {
+  if (winner == "user") {
+    userScore++;
+    userScoreBoard.innerText = userScore;
+    message.style.backgroundColor = "green";
+    message.innerText = `You won! ${userChoice} beats ${computerChoice}.`;
+  } else if (winner == "computer") {
+    computerScore++;
+    computerScoreBoard.innerText = computerScore;
+    message.style.backgroundColor = "red";
+    message.innerText = `You lost! ${computerChoice} beats ${userChoice}.`;
+  }
+};
+
+const playGame = (userChoice) => {
+  const computerChoice = getComputerChoice();
+  if (userChoice === computerChoice) {
+    gameDraw(userChoice);
+  } else {
+    const winner = decideWinner(userChoice, computerChoice);
+    showWinner(winner, userChoice, computerChoice);
+  }
+};
