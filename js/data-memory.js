0d6960196d37ffda8bae1fede39a214ed4060de3
Depkstha <dharmarajshrestha9870@gmail.com>
2025-01-11 23:40:55 +0545

    add initial JavaScript promises example and HTML setup for learning

---
+++ js/data-memory.js
@@ -0,0 +1,70 @@
+// console.log("Event Loop");
+
+// console.log("First log: started!");
+
+// setTimeout(() => {
+//   console.log("Second log: First timeout!");
+// }, 200);
+
+// setTimeout(() => {
+//   console.log("Third log: Second timeout!");
+// }, 100);
+
+// Promise.resolve().then(() => {
+//   console.log("Fourth log: Promise!");
+// });
+
+// console.log("Fifth log: finished!");
+
+// //Promises
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Second log: First timeout!");
+//     resolve();
+//   }, 200);
+// });
+
+const promise1 = new Promise((resolve, reject) => {
+    //DB calls, API calls, Reading and writing file, File upload, Error handling, Processing image, Lazy loading, Web socket or Event handling 
+    setTimeout(() => {
+        console.log("Async task is complete");
+        resolve();
+    })
+})
+
+promise1.then(() => {
+    console.log("Promise 1 resolved");
+})
+
+//Handling multiple execution 
+const task1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 1 is complete");
+        resolve();
+    })
+})
+
+const task2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 2 is complete");
+        resolve();
+    })
+})
+
+Promise.all([task1, task2]).then(() => {
+    console.log("All tasks are complete");
+})
+
+//Passing parameter in promise
+
+const task3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task 3 is complete");
+        resolve({name: "Deepak Shrestha", age: 26});
+    }, 1000)
+})
+
+task3.then((data) => {
+    console.log(data);
+})  
